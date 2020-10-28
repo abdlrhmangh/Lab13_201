859543f47c4bc2ceac6d0c96f3e078a863f835e1
abdlrhmangh <63260621+abdlrhmangh@users.noreply.github.com>
2020-10-28 17:45:48 +0300

    update_main.java
    implemented the same task using loops

--- Main.java
+++ Main.java
@@ -12,49 +12,39 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
-
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         GridPane pane = new GridPane();
         pane.setAlignment(Pos.CENTER);
-        pane.setPadding(new Insets(11.5,12.5,13.5,14.5));
+        pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
         pane.setHgap(5.5);
         pane.setVgap(5.5);
 
-        pane.add(new Button(Integer.toString(numberss())),0,0);
-        pane.add(new Button(Integer.toString(numberss())), 1, 0);
-        pane.add(new Button(Integer.toString(numberss())), 2, 0);
-        pane.add(new Button(Integer.toString(numberss())), 0, 1);
-        pane.add(new Button(Integer.toString(numberss())), 1, 1);
-        pane.add(new Button(Integer.toString(numberss())), 2, 1);
-        pane.add(new Button(Integer.toString(numberss())), 2, 2);
-        pane.add(new Button(Integer.toString(numberss())), 1, 2);
-        pane.add(new Button(Integer.toString(numberss())), 0, 2);
+        for (int i = 0; i < 3; i++) {
 
+            for (int w = 0; w < 3; w++) {
+                pane.add(new Button(Integer.toString(numberss())), i, w);
 
+             
+            }
 
-
+        }
         Scene scene = new Scene(pane);
         primaryStage.setTitle("ShowGridPane");
         primaryStage.setScene(scene);
         primaryStage.show();
 
 
-
     }
 
 
-
-    public int numberss(){
-        int x=(int)(Math.random()*8+1);
+    public int numberss() {
+        int x = (int) (Math.random() * 9 + 1);
 
         return x;
     }
-
-
-
 
 
     public static void main(String[] args) {
